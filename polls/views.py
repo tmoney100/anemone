2828cd5f31229d968fa6f04f328e61e136b55d49
Taylor Allred <taysky@taylors-mbp.lan>
2024-08-05 19:04:26 -0600

    change sqlite to postgres

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, HttpRequest
+
+
+def index(request: HttpRequest):
+    return HttpResponse("Hello, world. You're at the polls index.")
